436a1bdf6c12d1455f91258b23838d8306a8cf5c
원혁 이 <qnwnen0228@gmail.com>
2024-01-17 23:30:37 +0900

    Queue 4.3 4.4 추가

---
+++ DataStructure/Queue/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace DataStructure.Queue
+{
+    public class Node
+    {
+        public object Data { get; set; }
+        public Node Next { get; set; }
+        public Node(object data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
---
+++ DataStructure/Queue/QueueUsingLinkedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DataStructure.Queue
+{
+    public class QueueUsingLinkedList
+    {
+        private Node head = null;
+        private Node tail = null;
+
+        public QueueUsingLinkedList(object data)
+        {
+            if (head == null)
+            {
+                head = new Node(data);
+                tail = head;
+            }
+            else
+            {
+                tail.Next = new Node(data);
+                tail = tail.Next;
+            }
+        }
+
+        public object Dequeue()
+        {
+            if (head == null)
+            {
+                throw new Exception("Empty");
+            }
+
+            object data = head.Data;
+
+            if (head == tail)
+            {
+                head = tail = null;
+            }
+            else
+            {
+                head = head.Next;
+            }
+
+            return data;
+        }
+    }
+}
