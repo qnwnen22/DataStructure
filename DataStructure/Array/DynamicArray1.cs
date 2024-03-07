dc3311b10e354f1bd4e62b9fb48789c4c095adf9
이원혁 <LamySolution@DESKTOP-AROCTLG>
2024-03-07 16:48:11 +0900

    Array까지 복습 및 정리

--- DataStructure/Array/DynamicArray1.cs
+++ DataStructure/Array/DynamicArray1.cs
@@ -4,16 +4,21 @@
     {
         private object[] arr = new object[0];
 
+        // 동적 배열에 요소를 추가
         public void Add(object element)
         {
-            var temp = new object[arr.Length + 1];
+            object[] temp = new object[arr.Length + 1]; // 임시 배열 생성
+                                                        // 생성한 배열은 기존 배열의 +1 만큼 크게 생성
+
+            // temp에 기존 데이터 복사
             for (int i = 0; i < arr.Length; i++)
             {
                 temp[i] = arr[i];
             }
-            arr = temp;
 
-            arr[arr.Length - 1] = element;
+            arr = temp; // 복사한 temp를 기존 arr 필드에 초기화
+
+            arr[arr.Length - 1] = element; // 새로운 요소를 추가
         }
     }
 }
--- DataStructure/Array/DynamicArray2.cs
+++ DataStructure/Array/DynamicArray2.cs
@@ -3,11 +3,12 @@
     public class DynamicArray2
     {
         private object[] arr;
-        private const int GROWTH_FACTOR = 2;
+        private const int GROWTH_FACTOR = 2; // 동적으로 배열의 크기(배수)
+                                             // 2배만큼 동적 확장
 
-        public int Count { get; private set; }
+        public int Count { get; private set; } // 현재 배열요소의 개수
 
-        public int Capacity
+        public int Capacity // 최대수용 용량
         {
             get
             {
@@ -15,7 +16,7 @@
             }
         }
 
-        public DynamicArray2(int capacity = 16)
+        public DynamicArray2(int capacity = 16) // 기본 배열의 크기 = 16
         {
             arr = new object[capacity];
             Count = 0;
@@ -26,25 +27,28 @@
             // 배열이 찼을 때 확장
             if (Count >= Capacity)
             {
-                int newSize = Capacity * GROWTH_FACTOR;
-                var temp = new object[newSize];
+                int newSize = Capacity * GROWTH_FACTOR; // GROWTH_FACTOR의 배수만큼 최대수용 용량 지정
+                var temp = new object[newSize]; // 데이터를 복사할 배열 생성
+
                 for (int i = 0; i < arr.Length; i++)
                 {
-                    temp[i] = arr[i];
+                    temp[i] = arr[i]; // temp에 기존 데이터 복사
                 }
-                arr = temp;
+
+                arr = temp; // 기존 배열을 복사한 배열로 초기화
             }
 
-            arr[Count] = element;
-            Count++;
+            arr[Count] = element; // 추가할 요소를 배열 마지막에 추가
+            Count++; // 요소가 추가되었으니 카운터 증감
         }
 
         public object Get(int index)
         {
-            if(index > Capacity - 1)
+            if (index > Capacity - 1)
             {
                 throw new System.Exception("Element not found");
             }
+
             return arr[index];
         }
     }
