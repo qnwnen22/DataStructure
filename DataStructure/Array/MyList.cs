3ede3bf2d12860948217d0668bc5a62724242a63
이원혁 <LamySolution@DESKTOP-AROCTLG>
2024-05-02 13:07:27 +0900

    동적배열 구현학습

---
+++ DataStructure/Array/MyList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace DataStructure.Array
+{
+    public class MyList<T>
+    {
+        private T[] array;
+        private const int growthFactor = 2;
+        public int Capacity
+        {
+            get { return array.Length; }
+        }
+
+        public int Count { get; private set; }
+
+        public MyList(int capacity = 16)
+        {
+            array = new T[capacity];
+        }
+
+
+        public void Add(T t)
+        {
+            if (Count >= Capacity)
+            {
+                T[] tempArray = new T[Capacity * growthFactor]; ;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    tempArray[i] = array[i];
+                }
+                array = tempArray;
+            }
+
+            array[Count] = t;
+            Count++;
+        }
+
+        public T Get(int index)
+        {
+            if (index >= Count) throw new Exception("요소 없음");
+            return array[index];
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+
+        public void Remove(int index)
+        {
+            if (index >= Count) return;
+
+            if (index == Count - 1)
+            {
+                array[index] = default;
+            }
+            else
+            {
+                for (int i = index; i < Capacity; i++)
+                {
+                    T next = array[index + 1];
+                    array[index] = next;
+                    if (next == null) break;
+                }
+            }
+            Count--;
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -8,10 +8,99 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            Graph.Example.Example9();
+            var myLinkedList = new MyLinkedList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                myLinkedList.Add(i);
+            }
+            myLinkedList.Remove(5);
 
+            var linkedList = new System.Collections.Generic.LinkedList<int>();
+            linkedList.AddLast(1);
+            linkedList.AddLast(2);
+            linkedList.AddLast(3);
+            linkedList.AddLast(4);
+            linkedList.AddLast(3);
+            linkedList.AddLast(5);
+
+            linkedList.Remove(3);
+
+            var find =linkedList.Find(1);
+        }
+    }
+
+
+    public class MyNode<T>
+    {
+        public T value;
+        public MyNode<T> previous;
+        public MyNode<T> next;
+
+        public MyNode(T data)
+        {
+            this.value = data;
         }
 
+    }
 
+    public class MyLinkedList<T>
+    {
+        private MyNode<T> head;
+        public int Count { get; private set; }
+
+        public void Add(MyNode<T> newNode)
+        {
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                MyNode<T> current = head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+                current.next = newNode;
+                newNode.previous = current;
+            }
+            Count++;
+        }
+
+        public void Add(T data)
+        {
+            var newNode = new MyNode<T>(data);
+            Add(newNode);
+        }
+
+        public MyNode<T> Find(T data)
+        {
+            if (head == null || data == null) return null;
+            MyNode<T> current = head;
+
+            while (true)
+            {
+                if (current.value.Equals(data))
+                {
+                    return current;
+                }
+                if (current.next == null) return null;
+                current = current.next;
+            }
+        }
+
+        public void Remove(MyNode<T> removeNode)
+        {
+            if (head == null || removeNode == null) return;
+            removeNode.previous.next = removeNode.next;
+            removeNode.next.previous = removeNode.previous;
+            removeNode = null;
+            Count--;
+        }
+        public void Remove(T data)
+        {
+            MyNode<T> find = Find(data);
+            Remove(find);
+        }
     }
 }
