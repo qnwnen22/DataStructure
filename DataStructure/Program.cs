8f8dc25afaaff86580b00057b2fa65cf68d8010c
원혁 이 <qnwnen0228@gmail.com>
2024-06-30 20:40:43 +0900

    진입첨 코드 정리

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -8,27 +8,6 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            var myLinkedList = new MyLinkedList<int>();
-            for (int i = 0; i < 10; i++)
-            {
-                myLinkedList.Add(i);
-            }
-            myLinkedList.Remove(5);
-
-            var linkedList = new System.Collections.Generic.LinkedList<int>();
-            linkedList.AddLast(1);
-            linkedList.AddLast(2);
-            linkedList.AddLast(3);
-            linkedList.AddLast(4);
-            linkedList.AddLast(3);
-            linkedList.AddLast(5);
-
-            linkedList.Remove(3);
-
-            var find =linkedList.Find(1);
         }
     }
-
-
-    
 }
